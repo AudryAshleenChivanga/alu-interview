15f2cf72fd18b7cdfb696dc950539c4153a96fbc
AudryAshleenChivanga <a.chivanga@alustudent.com>
2024-01-21 04:08:59 -0800

    correction 2

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -36,7 +36,7 @@ def minOperations(n):
         # While n is divisible by i, divide n by i and
         # increment operations count
         while n % i == 0:
-            operations +=
+            operations +=i
             n //= i
 
     # If n is a prime number, add it to the operations count
